132fb61e5a86775d122e10048a158a8a6d34791c
ssut <ssut@ssut.me>
2015-06-30 22:51:55 +0900

    Add: plugin feature

--- lib/llama/plugin.rb
+++ lib/llama/plugin.rb
@@ -3,7 +3,6 @@ module Llama
     module ClassMethods
       attr_reader :matchers
 
-      # @api private
       def self.extended(by)
         by.instance_exec do
           @matchers = {}
@@ -16,8 +15,44 @@ module Llama
       end
     end
 
+    module InstanceMethods
+      attr_reader :threads
+
+      def initialize(bot=nil)
+        @bot = bot
+        @threads = ThreadGroup.new
+      end
+
+      def dispatch(msg)
+        self.class.matchers.each do |pattern, dest|
+          unless self.class.method_defined? dest
+            puts "method does not exist"
+            break
+          end
+
+          match = msg.raw.match(pattern)
+          next unless match
+          captures = match.captures ||= []
+          thread = Thread.new { self.send(dest, msg, captures) }
+          @threads.add(thread)
+        end
+      end
+
+      def unregister
+        @bot.plugins.unregister(self)
+      end
+
+      def stop(force=false)
+        @threads.each do |thread|
+          thread.join(10) unless force
+          thread.kill
+        end
+      end
+    end
+
     # @api private
     def self.included(by)
+      by.send(:include, InstanceMethods)
       by.extend(ClassMethods)
     end
   end
---
+++ lib/llama/plugin_list.rb
@@ -0,0 +1,37 @@
+module Llama
+  class PluginList < Array
+    def initialize(bot)
+      @bot = bot
+      @matchers = {}
+      super()
+    end
+
+    def register(plugin)
+      self << plugin.new(@bot)
+    end
+
+    def unregister(*plugins)
+      self.each do |plugin|
+        self.delete(plugin)
+      end
+    end
+
+    def threads
+      self.map { |p| p.threads }.flatten
+    end
+
+    def dispatch(msg)
+      threads = []
+
+      self.each do |plugin|
+        threads << plugin.dispatch(msg)
+      end
+
+      threads
+    end
+
+    def stop_all
+      self.each { |p| p.stop }
+    end
+  end
+end
